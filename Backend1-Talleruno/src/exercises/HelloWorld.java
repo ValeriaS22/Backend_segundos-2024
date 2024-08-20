e1184b476e29c6d2ed0ccb540b00504ad9b8fabe
ValeriaS22 <165791800+ValeriaS22@users.noreply.github.com>
2024-08-20 07:25:41 -0500

    Cambios nueva clase
    Nueva clase del dia 8 - 20 - 24

--- Backend1-Talleruno/src/exercises/HelloWorld.java
+++ Backend1-Talleruno/src/exercises/HelloWorld.java
@@ -5,6 +5,6 @@ public class HelloWorld
     public static void main(String [] args)
     {
         System.out.println("Hola Mundo");
-        System.out.println("Buenos dias");
+        System.out.println("Hola");
     }
 }
---
+++ Backend1-Talleruno/src/javanotes/Operador.java
@@ -0,0 +1,16 @@
+package javanotes;
+
+public class Operador {
+    public static void main(String[] args) {
+        // Aritmeticos (+.-,/,*, %)
+
+        int num1= 50;
+        int num2= 35;
+
+        int sum = num1 + num2;
+        System.out.println(sum);
+
+        int rest = num1 - num2;
+        System.out.println(rest);
+    }
+}
